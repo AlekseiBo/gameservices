699a2a3c986f924632f4359e3c65e342a7ba1646
AlekseiBo <alex.bobylev@gmail.com>
2023-04-11 11:40:55 +0200

    Avatar network sync added

--- Runtime/GameServices/Services/GameDataService/GameData.cs
+++ Runtime/GameServices/Services/GameDataService/GameData.cs
@@ -10,7 +10,7 @@ namespace GameServices
         public static bool isInitialized;
 
         private IStaticDataService staticData;
-        protected static Dictionary<TKey, ScriptableObject> gameData;
+        private static Dictionary<TKey, ScriptableObject> gameData;
 
         protected GameData(string resourcePath)
         {
@@ -35,7 +35,7 @@ namespace GameServices
             if (action != null) GetEntry<T>(key).RemoveSubscriber(action);
         }
 
-        protected static DataEntry<T> GetEntry<T>(TKey key)
+        private static DataEntry<T> GetEntry<T>(TKey key)
         {
             if (!gameData.TryGetValue(key, out var dataContainer)) return default;
             return dataContainer as DataEntry<T>;
