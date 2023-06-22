56921e49e39d197d79b69378edc089c492dcb53e
AlekseiBo <alex.bobylev@gmail.com>
2023-06-22 14:31:18 +0200

    Set frame rate code block added

---
+++ Runtime/GameServices/CodeBlocks/Initialization/SetFrameRate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Toolset;
+
+namespace GameServices.CodeBlocks
+{
+    [CreateAssetMenu(fileName = "Set Frame Rate", menuName = "Code Blocks/Initialization/Set Frame Rate", order = 0)]
+    public class SetFrameRate : CodeBlock
+    {
+        [SerializeField] private int frameRate = 60;
+        protected override void Execute()
+        {
+            Application.targetFrameRate = frameRate;
+            Complete(true);
+        }
+    }
+}
