693c6e167db2666bd4d08c2429891e54574b83d2
AlekseiBo <alex.bobylev@gmail.com>
2023-06-23 17:08:17 +0200

    Vivos on OSX

--- Runtime/GameServices/CodeBlocks/Initialization/RegisterVivoxProvider.cs
+++ Runtime/GameServices/CodeBlocks/Initialization/RegisterVivoxProvider.cs
@@ -6,8 +6,16 @@ namespace GameServices.CodeBlocks
     [CreateAssetMenu(fileName = "Register Vivox Provider", menuName = "Code Blocks/Initialization/Register Vivox Provider", order = 0)]
     public class RegisterVivoxProvider : CodeBlock
     {
+        [SerializeField] private bool excludeOSX = true;
+
         protected override void Execute()
         {
+            if (excludeOSX && Application.platform == RuntimePlatform.OSXEditor)
+            {
+                Complete(true);
+                return;
+            }
+
             if (Services.All.Single<IVivoxProvider>() == null)
                 Services.All.RegisterSingle<IVivoxProvider>(new VivoxProvider());
 
