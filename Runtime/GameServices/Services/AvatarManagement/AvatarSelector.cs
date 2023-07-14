b99235cdcf49e0925339e01659a86aad8bab3b62
AlekseiBo <alex.bobylev@gmail.com>
2023-07-14 12:36:59 +0200

    Avatar customization reset option added

--- Runtime/GameServices/Services/AvatarManagement/AvatarSelector.cs
+++ Runtime/GameServices/Services/AvatarManagement/AvatarSelector.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace GameServices
 {
     public class AvatarSelector<T>
     {
         public readonly Avatar Part;
-        public readonly int CurrentSkinColor;
+        public readonly int CurrentIndex;
         public readonly List<T> Palette;
         public readonly Action<Avatar, int> SetAction;
+        public readonly bool ResetEnabled;
+        public readonly Avatar ResetPart;
 
-        public AvatarSelector(Avatar part, int currentSkinColor, List<T> palette, Action<Avatar, int> setAction)
+        public AvatarSelector(Avatar part, int currentIndex, List<T> palette, Action<Avatar, int> setAction)
         {
             Part = part;
-            CurrentSkinColor = currentSkinColor;
+            CurrentIndex = currentIndex;
             Palette = palette;
             SetAction = setAction;
+            ResetEnabled = false;
+        }
+
+        public AvatarSelector(Avatar part, int currentIndex, List<T> palette, Action<Avatar, int> setAction, Avatar resetPart)
+        {
+            Part = part;
+            CurrentIndex = currentIndex;
+            Palette = palette;
+            SetAction = setAction;
+            ResetEnabled = true;
+            ResetPart = resetPart;
         }
     }
 }
