8ff882a9cc6219bc375938bb092412e959f81965
AlekseiBo <alex.bobylev@gmail.com>
2023-07-19 19:44:13 +0200

    Team changed to house

---
+++ Runtime/GameServices/Services/GameDataService/HouseColor.cs
@@ -0,0 +1,14 @@
+﻿namespace GameServices
+{
+    public enum HouseColor
+    {
+        None = 0,
+        Ruby = 1,
+        Citrine = 2,
+        Amber = 3,
+        Peridot = 4,
+        Emerald = 5,
+        Sapphire = 6,
+        Amethyst = 7
+    }
+}
---
+++ Runtime/GameServices/Services/GameDataService/HouseDataEntry.cs
@@ -0,0 +1,11 @@
+﻿using Toolset;
+using UnityEngine;
+
+namespace GameServices
+{
+    [CreateAssetMenu(fileName = "HouseDataEntry", menuName = "Game Data/House Color Entry", order = 0)]
+    public class HouseDataEntry : DataEntry<HouseColor>
+    {
+
+    }
+}
